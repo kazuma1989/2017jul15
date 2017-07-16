c0aff543bb2f7b9f45642ca2a9bbfc4bc107d37b
kazuma1989 <funifuni.1204@gmail.com>
2017-07-16 12:31:58 +0900

    ビルド時のバージョンをサーブレットから取得できるようにした
    デプロイがうまく行っている確認しやすくするため。
    src/main/resources の verison.properties が直接書き換わってしまうのは、できれば避けたいが、やり方が分かっていない。

--- src/main/java/com/github/kazuma1989/jetty/VersionServlet.java
+++ src/main/java/com/github/kazuma1989/jetty/VersionServlet.java
@@ -2,13 +2,14 @@ package com.github.kazuma1989.jetty;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ResourceBundle;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class TestServlet extends HttpServlet {
+public class VersionServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
@@ -17,8 +18,10 @@ public class TestServlet extends HttpServlet {
 			throws ServletException, IOException {
 		resp.setContentType("text/html; charset=UTF-8");
 
-		// GETメソッドのときは、パラメーターによらず Hello World! を返すのみ
+		// GETメソッドのときは、パラメーターによらずバージョン情報を返す
+		ResourceBundle bundle = ResourceBundle.getBundle("version");
+
 		PrintWriter out = resp.getWriter();
-		out.println("Hello World!");
+		out.println(bundle.getString("version"));
 	}
 }
